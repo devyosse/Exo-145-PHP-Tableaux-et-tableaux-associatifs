58686e339aafc68b7a4af48b58eb2f67e090b574
devyosse <narutoshippuden884@gmail.com>
2022-01-12 13:23:52 +0100

    exo 145

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
-
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits);
+echo " _ ";
+echo $fruits[1];
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " . $age['Ben'] . " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" .$x  . ", Value=" . $y;
 }
 
 
@@ -21,15 +22,27 @@ echo "<br><br>Ben is " .  . " years old.<br><br>";
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
+$result = array_values($colors);
+echo "<br><br>";
+print_r($result);
 
+echo "<br><br>";
+
+sort($colors);
+print_r($colors);
+
+echo "<br><br>";
+
+rsort($colors);
+print_r($colors);
+
+echo "<br><br>";
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
-
-
+sort($age);
+print_r($age);
 
 
 
